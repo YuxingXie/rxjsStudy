c5c9052e32b0b3f895a500805596adef9cd7d37c
xieyuxing <185246042@qq.com>
2018-11-11 11:23:09 +0800

    a

---
+++ chapter-10/LikeMulticast.js
@@ -0,0 +1,13 @@
+// import 'rxjs/add/observable/interval';
+// import 'rxjs/add/operator/take';
+const {Observable} = require('rxjs/Rx') ;
+require('rxjs/add/observable/interval');
+require('rxjs/add/operator/take');
+
+const tick$ = Observable.interval(1000).take(3);
+
+tick$.subscribe(value => console.log('observer 1:'+value));
+
+setTimeout(()=>{
+    tick$.subscribe(value => console.log('observer 2:'+value))
+},2000);
